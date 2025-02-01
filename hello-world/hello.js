0a40380b43280db07e6f7c9996829abb8214075f
this-is-emon <mofijul.islam.emon@gmail.com>
2025-02-01 12:56:02 +0600

    Addition to "simple node and npm intro" commit

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -1,6 +1,6 @@
 console.log("Hey there! I am JS");
 
 /* -- The below 2 lines wil not be printed -- */
-console.log(window); 
-console.log(alert("Hey!"));
+// console.log(window); 
+// console.log(alert("Hey!"));
 
