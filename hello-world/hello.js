f4106b337b169ff445f330b6791afe25464f524c
this-is-emon <mofijul.islam.emon@gmail.com>
2025-02-16 22:27:25 +0600

    Deleting some files

--- hello-world/hello.js
+++
@@ -1,6 +0,0 @@
-console.log("Hey there! I am JS");
-
-/* -- The below 2 lines wil not be printed -- */
-// console.log(window); 
-// console.log(alert("Hey!"));
-
--- hello-world/result.js
+++
@@ -1,5 +0,0 @@
-const {add,sub} = require("./math");
-
-console.log(add(10,5));
-console.log(sub(10,5));
-
